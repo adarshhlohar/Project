5aab6468fdb6ef1fbcc3a617092595a099532108
Adarsh Hari Lohar <117507791+adarshhlohar@users.noreply.github.com>
2023-09-02 23:43:45 +0530

    Student JDBC using hibernate 02-09-23

--- JSDBatchMumbaiJava/Advancejava/projectXmlEg/src/main/java/com/mavenproject/App.java
+++ JSDBatchMumbaiJava/Advancejava/projectXmlEg/src/main/java/com/mavenproject/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Student st = new Student();
+    	st.setId(101);
+    	st.setName("Adarsh");
+    	st.setCity("Pune");
+    	
+    	System.out.println(st);
     }
 }
